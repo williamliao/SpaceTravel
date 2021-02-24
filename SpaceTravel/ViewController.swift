59c878e39c970d25f270217f9893f34033a643be
william.liao <william.liao@mitake.com.tw>
2021-02-24 13:07:04 +0800

    Initial Commit

---
+++ SpaceTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceTravel
+//
+//  Created by 雲端開發部-廖彥勛 on 2021/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
